6babf5421f2027368851f0a5dca7b66b57a4870a
Jobansin <103522425+Jobansin@users.noreply.github.com>
2023-02-11 23:52:28 -0500

    My message

--- src/Profile.java
+++ src/Profile.java
@@ -3,28 +3,46 @@ public class Profile implements Comparable<Profile> {
     private String fname;
     private Date dob; //use the Date class described in (f)
 
-    public Profile(String firstName, String lastName, Date birthdate)
+    public Profile(String fname, String lname, Date dob)
     {
-        fname = firstName;
-        lname = lastName;
-        dob = birthdate;
+        this.fname = fname;
+        this.lname = lname;
+        this.dob = dob;
     }
 
     @Override
     public String toString()
     {
-        // return Profile info
-        return "";
+        return fname + " " + lname + " " + dob;
     }
     @Override
     public boolean equals(Object o)
     {
+        if(obj == this){
+            return true;
+        }
         // Compare two profiles
-        return false;
+        if(!(obj instanceof Profile)){
+            return false;
+        }
+        Profile temporary = (Profile) obj;
+        return fname.equals(temporary.getfname()) && lname.equals(temporary.getlname()) && dob.equals(temporary.getdob());
     }
     @Override
     public int compareTo(Profile p) {
         // return int when comparing students
         return 0;
+    }
+
+    public String getfname(){
+        return fname;
+    }
+
+    public String getlname(){
+        return lname;
+    }
+
+    public Date getdob(){
+        return dob;
     }
 }
--- src/Roster.java
+++ src/Roster.java
@@ -1,9 +1,25 @@
 public class Roster {
     private Student[] roster;
     private int size;
-    private int find(Student student) {} //search the given student in roster
-    private void grow() {} //increase the array capacity by 4
-    public boolean add(Student student){} //add student to end of array
+    private static final int NOT_VALID = -1;
+    private int find(Student student) {
+        for(int x = 0; x < size; x++){
+            if(roster[x].getProfile().equals(student.getProfile())){
+                return x;
+            }
+        }
+        return NOT_VALID;
+    } //search the given student in roster
+    private void grow() {
+        Student[] temporary = new Student[size + 4];
+        for (int x = 0; x < size; x++){
+            temporary[x] = roster[x];
+        }
+        roster = temporary;
+    } //increase the array capacity by 4
+    public boolean add(Student student){
+
+    } //add student to end of array
     public boolean remove(Student student){}//maintain the order after remove
     public boolean contains(Student student){} //if the student is in roster
     public void print () {} //print roster sorted by profiles
