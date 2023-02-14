71e0844d99a33eb03ebb8c56ed1bdbe23d8a6faa
Jobansin <103522425+Jobansin@users.noreply.github.com>
2023-02-13 22:14:06 -0500

    message

--- src/project1/Date.java
+++ src/project1/Date.java
@@ -11,7 +11,7 @@ public class Date implements Comparable<Date> {
     private static final int QUADRENNIAL = 4;
     private static final int CENTENNIAL = 100;
     private static final int QUATERCENTENNIAL = 400;
-    private static final int TWENTYTWENTYTHREE = 2023;
+    private static final int CURRENTYEAR = 2023;
     private static final int MAXIMUM_MONTH = 11;
     private static final int MINIMUM_MONTH = 0;
     private static final int MAXIMUM_DAY = 31;
@@ -39,7 +39,7 @@ public class Date implements Comparable<Date> {
 
     public boolean isValid() {
         int balancer = month - 1;//off set month
-        if (year > TWENTYTWENTYTHREE) {
+        if (year > CURRENTYEAR) {
             return false;
         }
         if (month < MINIMUM_MONTH || balancer > MAXIMUM_MONTH) {
@@ -90,21 +90,21 @@ public class Date implements Comparable<Date> {
             }
             return true;
         }
-        if (day == MAXIMUM_DAY && ((balancer == Calendar.JANUARY) || (balancer == Calendar.MARCH) ||
-                (balancer == Calendar.MAY) || (balancer == Calendar.JULY) || (balancer == Calendar.AUGUST) ||
-                (balancer == Calendar.OCTOBER) || (balancer == Calendar.DECEMBER))){
+        if (day == MAXIMUM_DAY && ((balancer == Calendar.JANUARY) || (balancer == Calendar.MARCH) || (balancer == Calendar.MAY) || (balancer == Calendar.JULY) || (balancer == Calendar.AUGUST) || (balancer == Calendar.OCTOBER) || (balancer == Calendar.DECEMBER))) {
             return true;
         }
-        if(day == MAXIMUM_DAY - 1 && balancer != Calendar.FEBRUARY + 1){
+        if (day == MAXIMUM_DAY - 1 && balancer != Calendar.FEBRUARY + 1) {
             return true;
         }
         return false;
     }//check if a date is a valid calendar date
+
     @Override
     public String toString() {
         // return project1.Date info
         return month + "/" + day + "/" + year;
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -116,46 +116,69 @@ public class Date implements Comparable<Date> {
         Date date = (Date) o;
         return year == date.year && month == date.month && day == date.day;
     }
-    public int getYear(){
+
+    public int getYear() {
         return year;
     }
-    public int getMonth(){
+
+    public int getMonth() {
         return month;
     }
-    public int getDay(){
+
+    public int getDay() {
         return day;
     }
+
     @Override
     public int compareTo(Date date) {
-        if(year > date.getYear()){
+        if (year > date.getYear()) {
             return BEFORE;
         }
-        if(year == date.getYear()){
-            if(month > date.getMonth()) {
+        if (year == date.getYear()) {
+            if (month > date.getMonth()) {
                 return BEFORE;
             }
-            if(month == date.getMonth()) {
+            if (month == date.getMonth()) {
                 if (day > date.getDay())
                     return BEFORE;
             }
-            if(day == date.getDay()) {
+            if (day == date.getDay()) {
                 return SAME;
             }
         }
         // return int when comparing dates
         return AFTER;
     }
+
     public static void main(String[] args) {
         // Test cases for Date
         // Test cases for project1.Date
-        System.out.println("Test1: The output should return true.");
-        Date date1 = new Date("02/29/1600");
+        System.out.println("Test1: The output should return false.");
+        Date date1 = new Date("02/29/2015"); // Tests if a non-leap year and having the day 29 in the month Feb is valid.
         System.out.println(date1.toString() + ": " + date1.isValid());
-        System.out.println("Test2: The output should return true.");
-        Date date2 = new Date("02/29/2012");
+
+        System.out.println("Test2: The output should return false.");
+        Date date2 = new Date("02/00/2020");// Tests if having a 0 number for the date is valid.
         System.out.println(date2.toString() + ": " + date2.isValid());
+
         System.out.println("Test3: The output should return false.");
-        Date date3 = new Date("02/00/2020");
+        Date date3 = new Date("13/01/2017");// Tests if having a month greater than 13 is valid.
         System.out.println(date3.toString() + ": " + date3.isValid());
+
+        System.out.println("Test4: The output should return false.");
+        Date date4 = new Date("01/01/-2000");// Tests if having a negative number for the year is valid.
+        System.out.println(date4.toString() + ": " + date4.isValid());
+
+        System.out.println("Test5: The output should return false.");
+        Date date5 = new Date("02/20/3000");// Tests if having a single number for the year is valid.
+        System.out.println(date5.toString() + ": " + date5.isValid());
+
+        System.out.println("Test6: The output should return true.");
+        Date date6 = new Date("02/29/2016");// Tests if a leap year and having the day 29 in the month Feb is valid.
+        System.out.println(date6.toString() + ": " + date6.isValid());
+
+        System.out.println("Test7: The output should return true.");
+        Date date7 = new Date("09/2/2022");// Tests if having the day 31 is valid for January, March, May, July, August, October, December is valid.
+        System.out.println(date7.toString() + ": " + date7.isValid());
     }
 }
